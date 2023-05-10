b8f8455a7122c4edf75d8be96714dec694d80509
pf4 <74263128+elydre@users.noreply.github.com>
2023-05-10 17:26:49 +0200

    snake case

--- src/gui.c
+++ src/gui.c
@@ -10,95 +10,94 @@
 #include <X11/Xatom.h>
 #include <X11/XKBlib.h>
 
-static Display *s_Display;
-static Window s_Window;
-static int s_Screen;
-static GC s_Gc;
-static XImage *s_Image;
+static Display *display;
+static Window window;
+static int screen;
+static GC gc;
+static XImage *image;
 
 extern uint32_t* gui_screen_buffer;
 
-static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
-static unsigned int s_KeyQueueWriteIndex = 0;
-static unsigned int s_KeyQueueReadIndex = 0;
+static uint16_t key_queue[KEYQUEUE_SIZE];
+static uint32_t key_queue_write_index = 0;
+static uint32_t key_queue_read_index = 0;
 
-static void add_key_to_queue(int pressed, unsigned int keyCode) {
-    unsigned char key = keyCode & 0xFF;
+static void add_key_to_queue(int pressed, uint32_t key_code) {
+    unsigned char key = key_code & 0xFF;
 
-    unsigned short key_data = (pressed << 8) | key;
+    uint16_t key_data = (pressed << 8) | key;
 
-    s_KeyQueue[s_KeyQueueWriteIndex] = key_data;
-    s_KeyQueueWriteIndex++;
-    s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
+    key_queue[key_queue_write_index] = key_data;
+    key_queue_write_index++;
+    key_queue_write_index %= KEYQUEUE_SIZE;
 }
 
 void gui_init() {
-    memset(s_KeyQueue, 0, KEYQUEUE_SIZE * sizeof(unsigned short));
+    memset(key_queue, 0, KEYQUEUE_SIZE * sizeof(uint16_t));
 
     // window creation
 
-    s_Display = XOpenDisplay(NULL);
+    display = XOpenDisplay(NULL);
 
-    s_Screen = DefaultScreen(s_Display);
+    screen = DefaultScreen(display);
 
-    int blackColor = BlackPixel(s_Display, s_Screen);
-    int whiteColor = WhitePixel(s_Display, s_Screen);
+    int blackColor = BlackPixel(display, screen);
+    int whiteColor = WhitePixel(display, screen);
 
     XSetWindowAttributes attr;
     memset(&attr, 0, sizeof(XSetWindowAttributes));
     attr.event_mask = ExposureMask | KeyPressMask;
-    attr.background_pixel = BlackPixel(s_Display, s_Screen);
+    attr.background_pixel = BlackPixel(display, screen);
 
-    int depth = DefaultDepth(s_Display, s_Screen);
+    int depth = DefaultDepth(display, screen);
 
-    s_Window = XCreateSimpleWindow(s_Display, DefaultRootWindow(s_Display), 0, 0, RESX, RESY, 0, blackColor, blackColor);
+    window = XCreateSimpleWindow(display, DefaultRootWindow(display), 0, 0, RESX, RESY, 0, blackColor, blackColor);
 
-    XSelectInput(s_Display, s_Window, StructureNotifyMask | KeyPressMask | KeyReleaseMask);
+    XSelectInput(display, window, StructureNotifyMask | KeyPressMask | KeyReleaseMask);
 
-    XMapWindow(s_Display, s_Window);
+    XMapWindow(display, window);
 
-    s_Gc = XCreateGC(s_Display, s_Window, 0, NULL);
+    gc = XCreateGC(display, window, 0, NULL);
 
-    XSetForeground(s_Display, s_Gc, whiteColor);
+    XSetForeground(display, gc, whiteColor);
 
-    XkbSetDetectableAutoRepeat(s_Display, 1, 0);
+    XkbSetDetectableAutoRepeat(display, 1, 0);
 
     // Wait for the MapNotify event
 
-    while(1) {
+    while (1) {
         XEvent e;
-        XNextEvent(s_Display, &e);
-        if (e.type == MapNotify)
-        {
+        XNextEvent(display, &e);
+        if (e.type == MapNotify) {
             break;
         }
     }
 
-    s_Image = XCreateImage(s_Display, DefaultVisual(s_Display, s_Screen), depth, ZPixmap, 0, (char *)gui_screen_buffer, RESX, RESX, 32, 0);
+    image = XCreateImage(display, DefaultVisual(display, screen), depth, ZPixmap, 0, (char *) gui_screen_buffer, RESX, RESX, 32, 0);
 }
 
 
 void gui_draw_frame() {
     int sym;
-    if (s_Display) {
-        while (XPending(s_Display) > 0) {
+    if (display) {
+        while (XPending(display) > 0) {
             XEvent e;
-            XNextEvent(s_Display, &e);
+            XNextEvent(display, &e);
             if (e.type == KeyPress) {
-                sym = XkbKeycodeToKeysym(s_Display, e.xkey.keycode, 0, 0);
-                // printf("KeyPress:%d sym:%d\n", e.xkey.keycode, sym);
+                sym = XkbKeycodeToKeysym(display, e.xkey.keycode, 0, 0);
+                // printf("KeyPress:%d sym:%d\n", e.xkey.key_code, sym);
                 add_key_to_queue(1, sym);
             }
             else if (e.type == KeyRelease) {
-                sym = XkbKeycodeToKeysym(s_Display, e.xkey.keycode, 0, 0);
-                // printf("KeyRelease:%d sym:%d\n", e.xkey.keycode, sym);
+                sym = XkbKeycodeToKeysym(display, e.xkey.keycode, 0, 0);
+                // printf("KeyRelease:%d sym:%d\n", e.xkey.key_code, sym);
                 add_key_to_queue(0, sym);
             }
         }
 
-        XPutImage(s_Display, s_Window, s_Gc, s_Image, 0, 0, 0, 0, RESX, RESY);
+        XPutImage(display, window, gc, image, 0, 0, 0, 0, RESX, RESY);
 
-        // XFlush(s_Display);
+        // XFlush(display);
     }
 
     // puts("frame");
@@ -118,14 +117,14 @@ uint32_t get_ticks() {
 }
 
 int get_key(int* pressed, unsigned char* Key) {
-    if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex) {
+    if (key_queue_read_index == key_queue_write_index) {
         //key queue is empty
 
         return 0;
     } else {
-        unsigned short key_data = s_KeyQueue[s_KeyQueueReadIndex];
-        s_KeyQueueReadIndex++;
-        s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
+        uint16_t key_data = key_queue[key_queue_read_index];
+        key_queue_read_index++;
+        key_queue_read_index %= KEYQUEUE_SIZE;
 
         *pressed = key_data >> 8;
         *Key = key_data & 0xFF;
@@ -135,5 +134,5 @@ int get_key(int* pressed, unsigned char* Key) {
 }
 
 void set_window_title(char *title) {
-    XChangeProperty(s_Display, s_Window, XA_WM_NAME, XA_STRING, 8, PropModeReplace, (const unsigned char *) title, strlen(title));
+    XChangeProperty(display, window, XA_WM_NAME, XA_STRING, 8, PropModeReplace, (const unsigned char *) title, strlen(title));
 }
